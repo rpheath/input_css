a6a7d97533949c4d65de795ef69072cadd0e66ef
rpheath <rpheath@gmail.com>
2008-04-18 14:27:06 -0400

    add specs using TagHelper#tag documentation examples

--- spec/input_css_spec.rb
+++ spec/input_css_spec.rb
@@ -1,8 +1,19 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-# need to finish (start) specs
 describe "InputCSS" do
-  it "should" do
-    # ...
+  include ActionView::Helpers::TagHelper
+  
+  describe "ActionView::Helpers" do    
+    describe "use examples shown in TagHelper#tag documentation" do
+      # (http://www.noobkit.com/show/ruby/rails/rails-stable/actionpack/actionview/helpers/taghelper/tag.html)
+      it "should not add a default class attribute to non-input tags" do
+        tag('br').should eql("<br />")
+        tag('br', nil, true).should eql("<br>")
+        tag('img', {:src => 'open & shut.png'}).
+          should eql("<img src=\"open &amp; shut.png\" />")
+        tag('img', {:src => 'open &amp; shut.png'}, false, false).
+          should eql("<img src=\"open &amp; shut.png\" />")
+      end
+    end
   end
 end
