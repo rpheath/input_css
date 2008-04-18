1ee8c632f9f9056caeb06729a56a472faa18b523
rpheath <rpheath@gmail.com>
2008-04-18 14:31:00 -0400

    added specs using FormTagHelper#text_field_tag documentation examples

--- spec/input_css_spec.rb
+++ spec/input_css_spec.rb
@@ -2,6 +2,7 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "InputCSS" do
   include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::FormTagHelper
   
   describe "ActionView::Helpers" do    
     describe "use examples shown in TagHelper#tag documentation" do
@@ -13,6 +14,26 @@ describe "InputCSS" do
           should eql("<img src=\"open &amp; shut.png\" />")
         tag('img', {:src => 'open &amp; shut.png'}, false, false).
           should eql("<img src=\"open &amp; shut.png\" />")
+      end
+    end
+    
+    describe "use examples shown in FormTagHelper#text_field_tag documentation" do  
+      # (http://www.noobkit.com/show/ruby/rails/rails-stable/actionpack/actionview/helpers/formtaghelper/text_field_tag.html)
+      it "should behave as expected (according to documentation) with the addition of default class" do
+        text_field_tag('name').
+          should eql("<input class=\"text\" id=\"name\" name=\"name\" type=\"text\" />")
+        text_field_tag('query', 'Enter your search query here').
+          should eql("<input class=\"text\" id=\"query\" name=\"query\" type=\"text\" value=\"Enter your search query here\" />")
+        text_field_tag('request', nil, :class => 'special_input').
+          should eql("<input id=\"request\" name=\"request\" type=\"text\" />")
+        text_field_tag('address', '', :size => 75).
+          should eql("<input class=\"text\" id=\"address\" name=\"address\" size=\"75\" type=\"text\" value=\"\" />")
+        text_field_tag('zip', nil, :maxlength => 5).
+          should eql("<input class=\"text\" id=\"zip\" maxlength=\"5\" name=\"zip\" type=\"text\" />")
+        text_field_tag('payment_amount', '$0.00', :disabled => true).
+          should eql("<input class=\"text\" disabled=\"disabled\" id=\"payment_amount\" name=\"payment_amount\" type=\"text\" value=\"$0.00\" />")
+        text_field_tag('ip', '0.0.0.0', :maxlength => 15, :size => 20, :class => 'ip-input').
+          should eql("<input id=\"ip\" maxlength=\"15\" name=\"ip\" size=\"20\" type=\"text\" value=\"0.0.0.0\" />")
       end
     end
   end
