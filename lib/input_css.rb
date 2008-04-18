6ad576c869bd24b801fc17974524322314d29c7f
rpheath <rpheath@gmail.com>
2008-04-17 23:38:50 -0400

    initial import

---
+++ lib/input_css.rb
@@ -0,0 +1,34 @@
+module ActionView
+  module Helpers
+    module TagHelper
+      # intercept #tag method to modify the 'options'
+      def tag_with_default_css(name, options=nil, open=false, escape=true)
+        options = css_options_for_tag(name, options)
+        tag_without_default_css(name, options, open, escape)
+      end
+      
+      # save the original method
+      alias_method_chain :tag, :default_css
+      
+      private
+        def css_options_for_tag(name, options)
+          options.symbolize_keys!
+          return options if options[:type] == 'hidden'
+
+          # alter CSS class based on type 
+          # (only for <input ... /> tags)
+          if name.to_s.downcase =~ /^input$/            
+            type, css = options[:type], options[:class]
+            type = 'text' if type == 'password'
+            options[:class] = "#{css.to_s} #{type.to_s}" unless css && css.split.include?(type)
+          end
+          options.stringify_keys!
+        end
+    end
+    
+    # ensure that the new #tag method is always called
+    class InstanceTag
+      alias_method :tag_without_error_wrapping, :tag_with_default_css
+    end
+  end
+end
---
+++ tasks/input_css_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :input_css do
+#   # Task goes here
+# end
