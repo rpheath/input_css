66ad59774b62b2d9c3c50e31c8815303d5279016
rpheath <rpheath@gmail.com>
2008-04-18 14:28:54 -0400

    added check for type='password' back in

--- lib/input_css.rb
+++ lib/input_css.rb
@@ -22,6 +22,7 @@ module ActionView
           # (only for <input ... /> tags)
           if name.to_s.downcase =~ /^input$/            
             type, css = options[:type], options[:class]
+            type = 'text' if type == 'password'
             options[:class] = "#{css.to_s} #{type.to_s}".gsub!(/^ /, '') unless css && css.split.include?(type)
           end
           options
