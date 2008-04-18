8f93b833a9669c86550c78eac18a5aaa50dab421
rpheath <rpheath@gmail.com>
2008-04-18 11:58:31 -0400

    swapped out test/unit for rspec (still need to write the specs)

---
+++ spec/input_css_spec.rb
@@ -0,0 +1,8 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+# need to finish (start) specs
+describe "InputCSS" do
+  it "should" do
+    # ...
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'rubygems'
+require 'action_controller'
+require 'action_view'
+require File.join(File.dirname(__FILE__), "../lib/input_css")
